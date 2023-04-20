c07eb90950b65e0248791737a78ba86accbe9fd3
mzdeeherself <mensahruthdede6@gmail.com>
2023-04-20 16:25:39 -0700

    32 bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 float c;
 long int d;
 long long int e;
-printf("Size of a char: %u bytes(s)\n", sizeof(a));
-printf("Size of an int: %u bytes(s)\n", sizeof(b));
-printf("Size of a long int: %u bytes(s)\n", sizeof(d));
-printf("Size of a long long int: %u bytes(s)\n", sizeof(e));
-printf("Size of a float: %u bytes(s)\n", sizeof(c));
+printf("Size of a char: %u byte(s)\n", sizeof(a));
+printf("Size of an int: %u byte(s)\n", sizeof(b));
+printf("Size of a long int: %u byte(s)\n", sizeof(d));
+printf("Size of a long long int: %u byte(s)\n", sizeof(e));
+printf("Size of a float: %u byte(s)\n", sizeof(c));
 return (0);
 }
